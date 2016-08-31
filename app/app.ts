aaaa1eb599eb7f00bb5e3e3dc11f646555d5bc70
Joseph Chingalo <profschingalo@gmail.com>
2016-08-31 15:19:51 +0300

    inittiate pages

---
+++ app/app.ts
@@ -0,0 +1,50 @@
+import { Component, ViewChild } from '@angular/core';
+import { ionicBootstrap, Platform, Nav } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+
+import { HomePage } from './pages/home/home';
+import { ReportAccidentPage } from './pages/report-accident/report-accident';
+import { ReportOffensePage } from './pages/report-offense/report-offense';
+import { DriverVerificationPage } from './pages/driver-verification/driver-verification';
+import { VehicleVerificationPage } from './pages/vehicle-verification/vehicle-verification';
+
+@Component({
+  templateUrl: 'build/app.html'
+})
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Report Accident', component: ReportAccidentPage},
+      { title: 'Report Offense', component: ReportOffensePage },
+      { title: 'Driver Verification', component: DriverVerificationPage},
+      { title: 'Vehicle Verification', component: VehicleVerificationPage}
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
+
+ionicBootstrap(MyApp);
---
+++ app/pages/vehicle-verification/vehicle-verification.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the VehicleVerificationPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/vehicle-verification/vehicle-verification.html',
+})
+export class VehicleVerificationPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
