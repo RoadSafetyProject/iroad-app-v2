6efc2ac6f497e6f95e39400ae58da4fabdf5d3bb
Joseph Chingalo <profschingalo@gmail.com>
2016-09-02 23:46:10 +0300

    get data base name

--- app/providers/app/app.ts
+++ app/providers/app/app.ts
@@ -12,28 +12,25 @@ import 'rxjs/add/operator/map';
 export class App {
 
   private formattedBaseUrl :string;
+  //private databaseName : string;
 
   constructor(private http: Http) {
   }
 
   getFormattedBaseUrl(url){
-
     this.formattedBaseUrl = "";
     let urlToBeFormatted : string ="",urlArray : any =[],baseUrlString : any;
-
     if (!(url.split('/')[0] == "https:" || url.split('/')[0] == "http:")) {
       urlToBeFormatted = "http://" + url;
     } else {
       urlToBeFormatted = url;
     }
-
     baseUrlString = urlToBeFormatted.split('/');
     for(let index in baseUrlString){
       if (baseUrlString[index]) {
         urlArray.push(baseUrlString[index]);
       }
     }
-
     this.formattedBaseUrl = urlArray[0] + '/';
     for (let i =0; i < urlArray.length; i ++){
       if(i != 0){
@@ -41,7 +38,13 @@ export class App {
       }
     }
     return Promise.resolve(this.formattedBaseUrl);
+  }
 
+  getDataBaseName(url){
+    console.log(url);
+    let databaseName = "";
+    databaseName = url.replace('://', '_').replace('/', '_').replace('.', '_').replace(':', '_');
+    return Promise.resolve(databaseName);
   }
 
 }
