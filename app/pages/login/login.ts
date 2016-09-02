ce6568558b57654909b0c71564c848ab7bf1fde2
Joseph Chingalo <profschingalo@gmail.com>
2016-09-02 23:45:47 +0300

    login page

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController ,Nav} from 'ionic-angular';
+import { NavController ,ToastController} from 'ionic-angular';
 
 import { HomePage } from '../home/home';
 
@@ -18,18 +18,40 @@ import { App } from '../../providers/app/app';
 })
 export class LoginPage {
 
-  loginData : any ={};
+  private loginData : any ={};
 
-
-  constructor(private navCtrl: NavController,public app : App) {
+  constructor(private navCtrl: NavController,public app : App,public toastCtrl: ToastController) {
     this.loginData.logoUrl = 'img/logo.png';
   }
 
   login(){
-    this.app.getFormattedBaseUrl(this.loginData.serveUrl)
-      .then(formattedBaseUrl => {
-        this.loginData.serveUrl = formattedBaseUrl;
-        console.log(this.loginData);
-      });
+    if(this.loginData.serveUrl){
+      this.app.getFormattedBaseUrl(this.loginData.serveUrl)
+        .then(formattedBaseUrl => {
+          this.loginData.serveUrl = formattedBaseUrl;
+          if(!this.loginData.username){
+            this.setToasterMessage('Please Enter username');
+          }else if (!this.loginData.password){
+            this.setToasterMessage('Please Enter password');
+          }else{
+            this.app.getDataBaseName(this.loginData.serveUrl).then(databaseName=>{
+              console.log(databaseName);
+              console.log(this.loginData);
+            });
+
+          }
+        });
+    }else{
+      this.setToasterMessage('Please Enter server url');
+    }
+
+  }
+
+  setToasterMessage(message){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
   }
 }
