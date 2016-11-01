67c1998ca346593d90a2af6aff0814554d0cbf3f
Joseph Chingalo <profschingalo@gmail.com>
2016-11-01 11:48:51 +0300

    add script for install necessary plugs

---
+++ installPlugins.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+ionic plugin add com.cordova.plugins.sms
+ionic plugin add cordova-plugin-app-version
+ionic plugin add cordova-plugin-camera
+ionic plugin add cordova-plugin-compat
+ionic plugin add cordova-plugin-console
+ionic plugin add cordova-plugin-device
+ionic plugin add cordova-plugin-file
+ionic plugin add cordova-plugin-geolocation
+ionic plugin add cordova-plugin-media-capture
+ionic plugin add cordova-plugin-splashscreen
+ionic plugin add cordova-plugin-statusbar
+ionic plugin add cordova-plugin-whitelist
+ionic plugin add cordova-sqlite-storage
+ionic plugin add ionic-plugin-keyboard
+ionic plugin add phonegap-plugin-barcodescanner
+
